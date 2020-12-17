4e19dc65492d72b767f83461e77b2cbd9c9706be
Chris Mellard <chrism@datacom.co.nz>
2020-12-17 23:51:06 +1300

    fix: added conditional check to dns output

--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,7 @@ output "name_servers" {
   value = var.enabled ? azurerm_dns_zone.dns.0.name_servers : []
 }
 output "resource_group_name" {
-  value = azurerm_resource_group.dns.0.name
+  value = var.enabled ? azurerm_resource_group.dns.0.name : ""
 }
 output "subscription_id" {
   value = data.azurerm_subscription.current.subscription_id
