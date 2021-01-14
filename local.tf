22df939858d4e519398d16ce845293286155b4a9
Chris Mellard <chrism@datacom.co.nz>
2021-01-14 22:07:25 +1300

    fix: sanitize variable values

--- local.tf
+++ local.tf
@@ -1,3 +1,3 @@
 locals {
-  resource_group_name = var.resource_group_name != "" ? var.resource_group_name : "rg-dns-${var.cluster_name}"
+  resource_group_name = var.resource_group_name != "" ? var.resource_group_name : "rg-dns-${join("", regexall("[A-Za-z0-9\\-_().]", var.cluster_name))}"
 }
